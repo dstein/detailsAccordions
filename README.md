d3d10196e07fcbcc3bea07b0de02b6b283ff3eb8
dstein <dstein555@gmail.com>
2024-01-09 16:36:23 -0600

    v1.0.1 update - readme for more info

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # detailsAccordions
 
-Version: 1.0.0
+Version: 1.0.1
 
 A JavaScript function that applies smooth transition to &lt;details> elements
 
-WARNING: version 1.0.0 only works with Chromium based browsers
+Example usage: 
+- https://codepen.io/dstein/pen/PoLzXqo
+
+Change log:
+- v1.0.0
+    - Established
+    - WARNING: version 1.0.0 only works with Chromium based browsers
+- v1.0.1
+    - Fixed cross browser compatibility issue
+    - Refactored uneccessary code
--- detailsAccordions.js
+++ detailsAccordions.js
@@ -1,41 +1,37 @@
 /**
  * Details Accordions
  * - Automatically converts <details> HTML elements to have smoother transitions
- * - WARNING: version 1.0.0 only works with Chromium based browsers
  *
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Dan Steinmiller, https://dstein.dev
  * License: MIT License
  */
 
 const detailsAcc = ( function() {
     "use strict";
-
+  
     const details = document.querySelectorAll('details');
+  
+    if ( details.length ) {
 
-    details.forEach( (e) => {
-        const detailsSummary = e.querySelector('summary');
-        const detailsContent = e.querySelector('.content');
-
-        let getCompSummaryStyles = window.getComputedStyle(detailsSummary);
-        let summaryMarginBtmOffset = parseInt(getCompSummaryStyles.marginBottom);
-        let summaryMarginTopOffset = parseInt(getCompSummaryStyles.marginTop);
-        let summaryMarginOutput = summaryMarginBtmOffset + summaryMarginTopOffset;
-
-        let summaryHeight = ( detailsSummary.offsetHeight + summaryMarginOutput );
-        let contentHeight = detailsContent.offsetHeight;
-        let fullHeight = summaryHeight + contentHeight;
-
-        e.style.height = `${summaryHeight}px`;
-
-        e.addEventListener('toggle', (event) => {
-
-            if ( e.open ) {
-                e.style.height = `${fullHeight}px`;
-            } else {
-                e.style.height = `${summaryHeight}px`;
-            }
+        details.forEach( (e) => {
+            const detailsSummary = e.querySelector('summary');
+            const detailsContent = e.querySelector('.content');
+            let summaryHeight = parseInt( detailsSummary.offsetHeight );
+      
+            e.style.height = `${summaryHeight}px`;
+      
+            e.addEventListener('toggle', (event) => {
+      
+                if ( e.open ) {
+                    let contentHeight = parseInt( detailsContent.offsetHeight );
+                    let fullHeight = ( summaryHeight + contentHeight );
+        
+                    e.style.height = `${fullHeight}px`;
+                } else {
+                    e.style.height = `${summaryHeight}px`;
+                }
+            });
         });
-    });
-
+    }
 })();
